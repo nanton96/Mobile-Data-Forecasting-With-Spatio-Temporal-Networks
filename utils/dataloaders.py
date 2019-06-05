53970073aaf885909f1b6f93c3f4ddb453809f23
nanton96 <nanton96@hotmail.com>
2019-06-05 19:48:41 +0100

    started working on pytorch CONVLSTM

---
+++ utils/dataloaders.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class MilanDataLoader():
+
+    def __init__(self):
+
+        DATA_DIR = 'data_toy/milan_processed_train.npz'
+        data_set = np.load(DATA_DIR)
+        self.x = np.expand_dims(data_set['x'],4).transpose(0,3,1,2,4)
+        self.y = np.expand_dims(data_set['y'],4).transpose(0,3,1,2,4)
+        print(self.x.shape[0])
+    
+    def __getitem__(self,index):
+
+        inputs = self.x[index]
+        predictions = self.y[index]
+
+        return inputs, predictions
+
+    def __len__(self):
+        return self.x.shape[0]
