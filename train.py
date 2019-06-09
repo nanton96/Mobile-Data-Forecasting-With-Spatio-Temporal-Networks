58706f2f3a9990f2bf494cf90e6996d2b3cab16c
nanton96 <nanton96@hotmail.com>
2019-06-09 20:50:45 +0100

    Update

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from utils.arg_extractor import get_args
 from utils.experiment_builder import ExperimentBuilder
 from utils.models import ConvLSTMModel
 import torch
+from torch.utils.data import DataLoader
 args, device = get_args()  # get arguments from command line
 rng = np.random.RandomState(seed=args.seed)  
 
@@ -12,16 +13,28 @@ args.toy = True
 train_data = dataloaders.MilanDataLoader(_set = 'train',toy = args.toy)
 valid_data = dataloaders.MilanDataLoader(_set = 'valid',toy = args.toy)
 test_data  = dataloaders.MilanDataLoader(_set = 'test', toy = args.toy)
-example_x, example_y = train_data.__getitem__(1)
-seq_start = example_x.shape[0]
-seq_length = seq_start + example_y.shape[0]
-model = ConvLSTMModel(input_size = args.image_height, seq_start = args.seq_start, seq_length = args.seq_length, batch_size = args.batch_size)
-experiment = ExperimentBuilder(network_model=model,
-                                    experiment_name=args.experiment_name,
-                                    num_epochs=args.num_epochs,
-                                    weight_decay_coefficient=args.weight_decay_coefficient,
-                                    continue_from_epoch=args.continue_from_epoch,
-                                    device=device,
-                                    train_data=train_data, val_data=valid_data,
-                                    test_data=test_data)  # build an experiment object
-experiment_metrics, test_metrics = experiment.run_experiment()
+
+for (x,y) in train_data:
+    print(x.shape)
+    print(y.shape)
+
+# trainloader = DataLoader(
+#         self_built_dataset,
+#         batch_size=args.batch_size,
+#         shuffle=True,
+#         num_workers=4,
+#         drop_last = True)
+
+# example_x, example_y = train_data.__getitem__(1)
+# seq_start = example_x.shape[0]
+# seq_length = seq_start + example_y.shape[0]
+# model = ConvLSTMModel(input_size = args.image_height, seq_start = args.seq_start, seq_length = args.seq_length, batch_size = args.batch_size)
+# experiment = ExperimentBuilder(network_model=model,
+#                                     experiment_name=args.experiment_name,
+#                                     num_epochs=args.num_epochs,
+#                                     weight_decay_coefficient=args.weight_decay_coefficient,
+#                                     continue_from_epoch=args.continue_from_epoch,
+#                                     device=device,
+#                                     train_data=train_data, val_data=valid_data,
+#                                     test_data=test_data)  # build an experiment object
+# experiment_metrics, test_metrics = experiment.run_experiment()
