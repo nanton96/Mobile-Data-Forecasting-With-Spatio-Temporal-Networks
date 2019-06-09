c725e2c02bfbcf27e711d823fd79f4914a155a78
nanton96 <nanton96@hotmail.com>
2019-06-09 20:55:27 +0100

    Update

--- train.py
+++ train.py
@@ -10,32 +10,45 @@ rng = np.random.RandomState(seed=args.seed)
 
 torch.manual_seed(seed=args.seed)
 args.toy = True
-train_data = dataloaders.MilanDataLoader(_set = 'train',toy = args.toy)
-valid_data = dataloaders.MilanDataLoader(_set = 'valid',toy = args.toy)
-test_data  = dataloaders.MilanDataLoader(_set = 'test', toy = args.toy)
+train_dataset = dataloaders.MilanDataLoader(_set = 'train',toy = args.toy)
+valid_dataset = dataloaders.MilanDataLoader(_set = 'valid',toy = args.toy)
+test_dataset  = dataloaders.MilanDataLoader(_set = 'test', toy = args.toy)
 
 
 
-trainloader = DataLoader(
+train_data = DataLoader(
         train_data,
         batch_size=args.batch_size,
         shuffle=True,
         num_workers=4,
         drop_last = True)
-for (x,y) in trainloader:
-    print(x.shape)
-    print(y.shape)
 
-# example_x, example_y = train_data.__getitem__(1)
-# seq_start = example_x.shape[0]
-# seq_length = seq_start + example_y.shape[0]
-# model = ConvLSTMModel(input_size = args.image_height, seq_start = args.seq_start, seq_length = args.seq_length, batch_size = args.batch_size)
-# experiment = ExperimentBuilder(network_model=model,
-#                                     experiment_name=args.experiment_name,
-#                                     num_epochs=args.num_epochs,
-#                                     weight_decay_coefficient=args.weight_decay_coefficient,
-#                                     continue_from_epoch=args.continue_from_epoch,
-#                                     device=device,
-#                                     train_data=train_data, val_data=valid_data,
-#                                     test_data=test_data)  # build an experiment object
-# experiment_metrics, test_metrics = experiment.run_experiment()
+
+val_data = DataLoader(
+        train_data,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=4,
+        drop_last = True)
+
+
+test_data = DataLoader(
+        train_data,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=4,
+        drop_last = True)
+
+example_x, example_y = train_data.__getitem__(1)
+seq_start = example_x.shape[0]
+seq_length = seq_start + example_y.shape[0]
+model = ConvLSTMModel(input_size = args.image_height, seq_start = args.seq_start, seq_length = args.seq_length, batch_size = args.batch_size)
+experiment = ExperimentBuilder(network_model=model,
+                                    experiment_name=args.experiment_name,
+                                    num_epochs=args.num_epochs,
+                                    weight_decay_coefficient=args.weight_decay_coefficient,
+                                    continue_from_epoch=args.continue_from_epoch,
+                                    device=device,
+                                    train_data=train_data, val_data=valid_data,
+                                    test_data=test_data)  # build an experiment object
+experiment_metrics, test_metrics = experiment.run_experiment()
