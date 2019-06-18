e4a8faf92c5386df6bb9ded12a9de69284c92e9b
Nick <nanton96@hotmail.com>
2019-06-19 00:23:54 +0100

    Update

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -20,9 +20,9 @@ class MilanDataLoader(data.Dataset):
         
         data_set = np.load(DATA_DIR)
         if create_channel_axis == True: 
-            self.x = np.expand_dims(data_set['x'],4).transpose(3,0,4,1,2).astype(np.float32)   
+            self.x = np.expand_dims(data_set['x'],4).transpose(0,3,4,1,2).astype(np.float32)   
             # DIMENSIONS S B C H W
-            self.y = np.expand_dims(data_set['y'],4).transpose(3,0,4,1,2).astype(np.float32)   
+            self.y = np.expand_dims(data_set['y'],4).transpose(0,3,4,1,2).astype(np.float32)   
             # seq_len batch_size input_channel height width
         
         else:
