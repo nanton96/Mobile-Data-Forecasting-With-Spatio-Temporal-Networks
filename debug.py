3b05cd6bdc7f89a3af622d04ce57ca30c3a3e194
Nick <nanton96@hotmail.com>
2019-06-19 00:58:23 +0100

    Update

--- debug.py
+++ debug.py
@@ -1,25 +1,54 @@
-import sys
-sys.path.insert(0, '../')
-import torch
-from collections import OrderedDict
-from utils.new_models import EF,Encoder,Forecaster,ConvLSTM
+# import sys
+# sys.path.insert(0, '../')
+# import torch
+# from collections import OrderedDict
+# from utils.new_models import EF,Encoder,Forecaster,ConvLSTM
+# import utils.dataloaders as dataloaders
+# batch_size = 10
+# encoder_architecture = [
+#     [   OrderedDict({'conv1_leaky_1': [1, 8, 4, 2, 1]}),
+#         OrderedDict({'conv2_leaky_1': [64, 192, 4, 2, 1]}),
+#         OrderedDict({'conv3_leaky_1': [192, 192, 3, 2, 1]}),
+#     ],
+
+#     [
+#         ConvLSTM(input_channel=8, num_filter=64, b_h_w=(batch_size, 50, 50),
+#                  kernel_size=3, stride=1, padding=1),
+#         ConvLSTM(input_channel=192, num_filter=192, b_h_w=(batch_size, 25, 25),
+#                  kernel_size=3, stride=1, padding=1),
+#         ConvLSTM(input_channel=192, num_filter=192, b_h_w=(batch_size, 13, 13),
+#                  kernel_size=3, stride=1, padding=1),
+#     ]
+# ]
+# encoder = Encoder(encoder_architecture[0],encoder_architecture[1])
+
+# encoder
+
+
 import utils.dataloaders as dataloaders
-batch_size = 10
-encoder_architecture = [
-    [   OrderedDict({'conv1_leaky_1': [1, 8, 4, 2, 1]}),
-        OrderedDict({'conv2_leaky_1': [64, 192, 4, 2, 1]}),
-        OrderedDict({'conv3_leaky_1': [192, 192, 3, 2, 1]}),
-    ],
+import numpy as np
+from utils.arg_extractor import get_args
+from utils.new_experiment_builder import ExperimentBuilder
+from utils.new_models import EF,Encoder,Forecaster,ConvLSTM
+import torch
+from torch.utils.data import DataLoader
+args, device = get_args()  # get arguments from command line
+rng = np.random.RandomState(seed=args.seed)  
+from collections import OrderedDict
 
-    [
-        ConvLSTM(input_channel=8, num_filter=64, b_h_w=(batch_size, 50, 50),
-                 kernel_size=3, stride=1, padding=1),
-        ConvLSTM(input_channel=192, num_filter=192, b_h_w=(batch_size, 25, 25),
-                 kernel_size=3, stride=1, padding=1),
-        ConvLSTM(input_channel=192, num_filter=192, b_h_w=(batch_size, 13, 13),
-                 kernel_size=3, stride=1, padding=1),
-    ]
-]
-encoder = Encoder(encoder_architecture[0],encoder_architecture[1])
+torch.manual_seed(seed=args.seed)
+args.toy = False
+batch_size = args.batch_size
 
-encoder
+train_dataset = dataloaders.MilanDataLoader(_set = 'train',toy = args.toy,create_channel_axis=True)
+valid_dataset = dataloaders.MilanDataLoader(_set = 'valid',toy = args.toy,create_channel_axis=True)
+test_dataset  = dataloaders.MilanDataLoader(_set = 'test', toy = args.toy,create_channel_axis=True)
+
+train_data = DataLoader(train_dataset,batch_size=args.batch_size,shuffle=True,num_workers=4,drop_last = True)
+valid_data = DataLoader(valid_dataset,batch_size=args.batch_size,shuffle=True,num_workers=4,drop_last = True)
+test_data = DataLoader(test_dataset,batch_size=args.batch_size,shuffle=True,num_workers=4,drop_last = True)
+
+
+for x,y in train_data:
+    print(x.shape)
+    print(y.shape)
