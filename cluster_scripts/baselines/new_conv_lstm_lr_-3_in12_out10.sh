7dbeebc062c6348fe8f4a3fb19551f4cc697f1f7
nanton96 <nanton96@hotmail.com>
2019-06-24 21:16:45 +0100

    Update

--- cluster_scripts/baselines/new_conv_lstm_lr_-3_in12_out10.sh
+++ cluster_scripts/baselines/new_conv_lstm_lr_-3_in12_out10.sh
@@ -42,4 +42,5 @@ python train.py --use_gpu True --gpu_id "0" --num_epochs 100\
                 --toy False --learning_rate 0.001\
                 --weight_decay_coefficient 0\
                 --experiment_name new_conv_lstm_lr_-3_in12_out10.\
-                --model deepconvlstm
+                --model deepconvlstm\
+                --seq_start 12 --seq_length 22
