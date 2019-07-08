522fc5f96673d6e62746b244f54fe6d3b398eb3d
Nick <nanton96@hotmail.com>
2019-07-08 20:12:14 +0300

    Update

--- cluster_scripts/baselines/conv_lstm_lr_-3_in12_out10_no_shuffle_before_split.sh
+++ cluster_scripts/baselines/conv_lstm_lr_-3_in12_out10_no_shuffle_before_split.sh
@@ -43,5 +43,4 @@ python train.py --use_gpu True --gpu_id "0" --num_epochs 100\
                 --weight_decay_coefficient 0\
                 --experiment_name conv_lstm_lr_-3_in12_out10_no_shuffle_before_split\
                 --model shallowconvlstm\
-                --seq_start 12 --seq_length 22
-                --batch_size 5
+                --seq_start 12 --seq_length 22 --batch_size 5
