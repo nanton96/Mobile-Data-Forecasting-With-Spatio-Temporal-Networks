4be87d671782497892095eba9c8ebfc3c7f43443
Nick <nanton96@hotmail.com>
2019-07-14 20:21:57 +0300

    Update

--- test_predrnn.py
+++ test_predrnn.py
@@ -3,17 +3,18 @@ import torch
 import utils
 
 device = torch.cuda.current_device()
-x = torch.randn([5,12,100,100]).to(device)
-y = torch.randn([5,10,100,100]).to(device)
+x = torch.randn([5,12,50,50]).to(device)
+y = torch.randn([5,10,50,50]).to(device)
 
 num_hidden = [16,32,32,32]
 
 model = PredRNNPP(x.shape,12,10,5,num_hidden,device)
-
+torch.cuda
+print(torch.cuda.max_memory_allocated)
 model.to(device)
-
+print(torch.cuda.max_memory_allocated)
 out = model.forward(x)
-
+print(torch.cuda.max_memory_allocated)
 print('out shape' ,out.shape)
 
 se = torch.sum((out - y)**2,(2,3)) # MSE error per frame
