338aca0bef6fe569e8189690362915ff67aa4b18
Nick <nanton96@hotmail.com>
2019-07-17 19:46:00 +0300

    Update

--- test_predrnn_less_mem.py
+++ test_predrnn_less_mem.py
@@ -1,4 +1,4 @@
-from model_architectures.pred_rnn_pp.PredRNNPP import PredRNNPP
+from model_architectures.pred_rnn_pp.PredRNNPP_less_mem import PredRNNPP
 # from model_architectures.conv_lstm_deep.DeepConvLstm import 
 import torch 
 import utils
@@ -7,10 +7,13 @@ device = torch.cuda.current_device()
 x = torch.randn([5,12,50,50]).to(device)
 y = torch.randn([5,10,50,50]).to(device)
 
-num_hidden = [16,32,32,32]
+num_hidden = [16,32,32]
 
-model = PredRNNPP(x.shape,12,10,5,num_hidden,device)
-torch.cuda
+strides = [2,2,1]
+padding = [1,0,1]
+kernel_sizes = [4,3,3]
+
+model = PredRNNPP(x.shape,12,10,5,num_hidden,strides,padding,kernel_sizes,device)
 print(torch.cuda.max_memory_allocated())
 model.to(device)
 print(torch.cuda.max_memory_allocated())
