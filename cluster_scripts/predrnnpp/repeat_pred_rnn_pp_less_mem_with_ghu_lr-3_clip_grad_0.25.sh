95f5921fa0d0bf2bc8ad3f6734313bc9172263e3
Nick <nanton96@hotmail.com>
2019-07-20 14:23:54 +0100

    Update

--- cluster_scripts/predrnnpp/repeat_pred_rnn_pp_less_mem_with_ghu_lr-3_clip_grad_0.25.sh
+++ cluster_scripts/predrnnpp/repeat_pred_rnn_pp_less_mem_with_ghu_lr-3_clip_grad_0.25.sh
@@ -1,7 +1,8 @@
 /#!/bin/sh
 
-
-for i in {0..88..8}
+sbatch --export=start_epoch=-1 cluster_scripts/predrnnpp/pred_rnn_pp_less_mem_with_ghu_lr-3_clip_grad_0.25.sh
+sleep 2h
+for i in {7..87..8}
     do
     sbatch --export=start_epoch=$i cluster_scripts/predrnnpp/pred_rnn_pp_less_mem_with_ghu_lr-3_clip_grad_0.25.sh
     sleep 2h
